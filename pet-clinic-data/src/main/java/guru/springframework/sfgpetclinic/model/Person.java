e8a7e3aad2bcbafcb245c1b084927802c3c48c08
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-02 11:03:34 -0300

    Added interfaces. Closes #5

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 public class Person {
+
     private String firstName;
     private String lastName;
 
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -9,5 +9,4 @@ public class SfgPetClinicApplication {
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
     }
-
 }
