e492bc1b675388f17d03f0480da6bebdfea9e38d
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-03 10:14:55 -0300

    Fix

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Vet extends BaseEntity {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
