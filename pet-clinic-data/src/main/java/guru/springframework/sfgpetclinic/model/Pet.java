b716d2de240a05f2d71e5b1d1bfb083c9ae2b04c
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-03 10:13:40 -0300

    Impl map based Services

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet extends BaseEntity {
+public class Vet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
