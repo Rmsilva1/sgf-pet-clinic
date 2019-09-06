a86eaadd5585bdab9ac3814711917e39ea80b94c
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-06 08:02:32 -0300

    Added PetTypes to bootstrap. closes #31

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class PetType extends BaseEntity{
+public class PetType extends BaseEntity {
 
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
