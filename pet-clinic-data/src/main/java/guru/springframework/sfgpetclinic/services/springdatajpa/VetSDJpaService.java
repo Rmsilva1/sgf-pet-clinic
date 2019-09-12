652be9d7a6a0eab5fc101e9fff1577cfa7a37bca
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-12 14:32:08 -0300

    Added spring data Jpa service for Pet Type. Closes #42

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -21,9 +21,9 @@ public class VetSDJpaService implements VetService {
 
     @Override
     public Set<Vet> findAll() {
-        Set<Vet> vetSet = new HashSet<>();
-        vetRepository.findAll().forEach(vetSet::add);
-        return vetSet;
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
     }
 
     @Override
