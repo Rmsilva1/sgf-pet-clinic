6482114098ecf27a9fe8d90bb7f0427efb9b0570
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-12 13:14:49 -0300

    Implemented repos. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetRepository extends CrudRepository<Pet, Long> { }
