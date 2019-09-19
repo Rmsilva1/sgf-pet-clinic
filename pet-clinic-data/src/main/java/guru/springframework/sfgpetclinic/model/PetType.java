cd4403055bdc230e955f9e774f90cbd0a8485135
Rafael Mateus <rafael.mateus.silva@hotmail.com>
2019-09-19 16:24:41 -0300

    Adding builder pattern to PetType and tests. #56

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -15,6 +15,13 @@ import javax.persistence.Table;
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+
     @Column(name = "name")
     private String name;
 
